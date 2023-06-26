d49d46206e79f84ac42a4194afbb3d75676614db
dani voro <mr.voro@gmail.com>
2023-06-26 15:57:06 +0200

    solving the exercise

--- src/count-of-positives.test.ts
+++ src/count-of-positives.test.ts
@@ -1,10 +1,9 @@
-import countPositivesSumNegatives from "./count-of-positives";
+import countPositivesSumNegatives from './count-of-positives';
 
-test("sum adds two numbers", () => {
+test('sum adds two numbers', () => {
   expect(countPositivesSumNegatives(4, 3)).toBe(7);
 });
 
-
-test("sum adds two numbers cancelling", () => {
+test('sum adds two numbers cancelling', () => {
   expect(countPositivesSumNegatives(-4, 4)).toBe(0);
 });
