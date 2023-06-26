1e907ddc58a9b9057a0df8faf6b3c11164a38037
dani voro <mr.voro@gmail.com>
2023-06-26 16:04:50 +0200

    passing the tests

--- src/count-of-positives.test.ts
+++ src/count-of-positives.test.ts
@@ -1,9 +1,9 @@
 import countPositivesSumNegatives from './count-of-positives';
 
-test('sum adds two numbers', () => {
-  expect(countPositivesSumNegatives(4, 3)).toBe(7);
+test('testing some random numbers', () => {
+  expect(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])).toStrictEqual([10, -65]);
 });
 
 test('sum adds two numbers cancelling', () => {
-  expect(countPositivesSumNegatives(-4, 4)).toBe(0);
+  expect(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])).toStrictEqual([8, -50]);
 });
